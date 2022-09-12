e7d528d1c2191311fe63d3f0865b231763825b0f
playmaker159 <ashwinprabhu2001@gmail.com>
2022-09-12 21:39:56 +0530

    navbar fixed

--- navbar.js
+++ navbar.js
@@ -1,17 +1,10 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const toggle=document.getElementsByClassName('toggle-button')[0]
+const navbarLinks=document.getElementsByClassName('navbar-links')[0]
 
-hamburger.addEventListener("click", mobileMenu);
+toggle.addEventListener('click', ()=>{
+    navbarLinks.classList.toggle('active')
+})
 
-function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}
-const navLink = document.querySelectorAll(".nav-link");
-
-navLink.forEach(n => n.addEventListener("click", closeMenu));
-
-function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}
+navbarLinks.addEventListener('click', ()=>{
+    navbarLinks.classList.toggle('active')
+})
