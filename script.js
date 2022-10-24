b8f3d84ff488d051d82f17b706e78956137e60e2
playmaker159 <ashwinprabhu2001@gmail.com>
2022-10-24 14:16:46 +0530

    loding fade out animation

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-window.addEventListener('load',()=>{
-    setTimeout(()=>{
-        const loading=document.querySelector('.loading')
-    loading.style.display="none"
-    },2000)
+window.addEventListener('load', () => {
+    var val = 1
+    setTimeout(() => {
+        const loading = document.querySelector('.loading')
+        loading.id = "hidden"
+        if (loading.style.opacity == 0) {
+            console.log('yes');
+            setTimeout(() => {
+                loading.style.display = "none"
+            }, 2000)
+        }
+    }, 2000)
 })
+
+const loading = document.querySelector('.loading')
